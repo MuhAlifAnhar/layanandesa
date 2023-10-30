dbfa4fb800aa1fdd0dade57f4435df975181b9c2
Muh. Alif Anhar <sultankautsar21@gmail.com>
2023-10-30 10:10:34 +0700

    menambahkan loop di animasi dan kembali ke halaman awal jika tidak ada aksi selama satu menit

---
+++ datang.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function() {
+var idleTime = 0;
+
+function resetIdleTime() {
+    idleTime = 0;
+}
+
+document.addEventListener("mousemove", resetIdleTime);
+document.addEventListener("keypress", resetIdleTime);
+
+setInterval(function() {
+    idleTime++;
+    if (idleTime >= 60) {
+        window.location.href = "video.html";
+    }
+}, 1000);
+
+var idleTimee = 0;
+
+function resetIdleTimee() {
+    idleTimee = 0;
+}
+
+document.addEventListener("mousemove", resetIdleTimee);
+document.addEventListener("keypress", resetIdleTimee);
+
+setInterval(function() {
+    idleTimee++;
+    if (idleTimee >= 10) {
+        window.location.href = "datang.html";
+    }
+}, 1000);
+
+});
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var idleTime = 0;
+    
+//     function resetIdleTime() {
+//         idleTime = 0;
+//     }
+    
+//     document.addEventListener("mousemove", resetIdleTime);
+//     document.addEventListener("keypress", resetIdleTime);
+    
+//     setInterval(function() {
+//         idleTime++;
+//         if (idleTime >= 60) {
+//             window.location.href = "video.html";
+//         }
+//     }, 1000);
+    
+//     function restartAnimations() {
+//         var elements = document.querySelectorAll('[data-animate]');
+//         for(const el of Array.from(elements)) {
+//             const animateClass = el.dataset.animate
+//             el.classList.remove(animateClass)
+//             requestAnimationFrame(() => {
+//                 el.classList.add(animateClass)
+//             })
+//         }
+//     }
+    
+//         setInterval(restartAnimations, 2000);
+//     });
